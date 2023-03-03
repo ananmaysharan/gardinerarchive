eceeb134d1bb88a5a47d2bdc97316494726abb75
Ananmay Sharan <32856566+ananmaysharan@users.noreply.github.com>
2023-03-02 20:27:59 -0500

    added bentway zoom

--- script_past.js
+++ script_past.js
@@ -311,6 +311,33 @@ map.on("load", async () => {
         },
     });
 
+    // INDIGENOUS TERRITORIES ZOOMING
+    const start = {
+        center: [-79.380331532, 43.646497454], // // starting center in [lng, lat]
+        zoom: 13.6,
+    };
+
+    const end = {
+        center: [-79.380331532, 43.646497454],
+        zoom: 7,
+    };
+
+    let isAtStart = true;
+
+    document.getElementById('fly').addEventListener('click', () => {
+        // Zoom out to show Indigenous Territories
+        const target = isAtStart ? end : start;
+        isAtStart = !isAtStart;
+
+
+        map.flyTo({
+            ...target,
+            center: [-79.380331532, 43.646497454], //NE coordinates
+            duration: 1000,
+            essential: true // this animation is considered essential with respect to prefers-reduced-motion
+        });
+    });
+
 
     //  adding popup
 
@@ -351,9 +378,9 @@ map.on("load", async () => {
 
     document.getElementById('info').innerHTML = `
     <svg width="375px" height="24px" viewBox="0 0 330 22" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-    <title>freecodeformat</title>
+    <title>logo</title>
     <g id="Page-1" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
-        <g id="freecodeformat" fill="#6ECCB1" fill-rule="nonzero">
+        <g id="logo" fill="#6ECCB1" fill-rule="nonzero">
             <path d="M0,14.073957 C0,16.3945269 0.705934458,18.2931505 2.11775081,19.7698817 C3.52961973,21.2465591 5.35324433,22 7.61805825,22 C9.88287218,22 11.7064968,21.2465591 13.1183131,19.7698817 C14.530182,18.2931505 15.2361165,16.3945269 15.2361165,14.073957 L15.2361165,0.452043015 L11.2653074,0.452043015 L11.2653074,14.2548064 C11.2653074,16.5452258 10.0593689,18.0822043 7.61805825,18.0822043 C5.17674757,18.0822043 3.97080906,16.5753226 3.97080906,14.1945054 L3.97080906,0.452043015 L0,0.452043015 L0,14.073957 Z M24.2105928,21.547957 L24.2105928,8.01644086 L31.5933396,21.547957 L35.2111551,21.547957 L35.2111551,0.452043015 L31.210965,0.452043015 L31.210965,13.9835591 L23.9164666,0.452043015 L20.2397837,0.452043015 L20.2397837,21.547957 L24.2105928,21.547957 Z M43.9798061,4.36989246 L47.4799396,4.36989246 C48.8035602,4.36989246 49.6859914,4.82193548 50.1859954,5.69587097 C50.6859995,6.56986022 50.9507446,8.2273871 50.9507446,10.6685054 C50.8624963,16.5150753 49.950684,17.6301613 47.3034954,17.6301613 L43.9798061,17.6301613 L43.9798061,4.36989246 Z M47.3034954,21.547957 C52.3919946,21.547957 54.9216063,17.9315054 54.9216063,10.6685054 C54.9216063,3.85754838 52.4508094,0.452043015 47.4799396,0.452043015 L40.008997,0.452043015 L40.008997,21.547957 L47.3034954,21.547957 Z M73.7791124,21.547957 L73.7791124,17.6301613 L64.7198567,17.6301613 L64.7198567,12.4164301 L72.9554959,12.4164301 L72.9554959,8.49863441 L64.7198567,8.49863441 L64.7198567,4.36989246 L73.7791124,4.36989246 L73.7791124,0.452043015 L60.7489425,0.452043015 L60.7489425,21.547957 L73.7791124,21.547957 Z M84.0183419,21.547957 L84.0183419,13.2602688 L86.3714568,13.2602688 L90.1952027,21.547957 L94.607096,21.547957 L90.4595799,12.5369785 C92.7243939,11.2410968 93.7246122,9.52326882 93.7246122,6.84110752 C93.7246122,5.03288172 93.1070313,3.52605375 91.8713437,2.29041935 C90.6656155,1.05478495 89.1949844,0.452043015 87.4594505,0.452043015 L80.0474277,0.452043015 L80.0474277,21.547957 L84.0183419,21.547957 Z M84.0183419,4.36989246 L87.4594505,4.36989246 C88.6657044,4.36989246 89.753698,5.36437635 89.753698,6.90135484 C89.6653971,8.95067742 88.2536333,9.40272044 87.4594505,9.34247312 L84.0183419,9.34247312 L84.0183419,4.36989246 Z M129.645223,14.284957 C129.61579,16.5753226 128.438969,18.0822043 125.998079,18.0822043 C124.674091,18.0822043 123.733265,17.5397097 123.174026,16.4849247 C122.615312,15.4301398 122.320976,13.4712151 122.320976,10.6685054 C122.320976,5.72602151 123.409495,3.9177957 125.998079,3.9177957 C128.204026,3.9177957 129.409754,5.36437635 129.674657,8.2273871 L133.380668,7.71509676 C133.2335,5.51507527 132.468751,3.67669893 131.086421,2.2000215 C129.733524,0.723290322 128.027424,0 125.998079,0 C121.29185,0 118.350587,3.64660214 118.350587,10.6685054 C118.350587,18.4136989 121.203549,22 125.998079,22 C128.262893,22 130.086202,21.2465591 131.497966,19.7698817 C132.90973,18.2931505 133.616137,16.3945269 133.616137,14.073957 L133.616137,10.5178065 L125.086162,10.5178065 L125.086162,14.284957 L129.645223,14.284957 Z M140.79637,21.547957 L142.002099,17.8712043 L149.032009,17.8712043 L150.237738,21.547957 L154.473555,21.547957 L147.355343,0.452043015 L143.855367,0.452043015 L136.707722,21.547957 L140.79637,21.547957 Z M145.531508,6.72055914 L147.767414,13.9534086 L143.325561,13.9534086 L145.531508,6.72055914 Z M162.859516,21.547957 L162.859516,13.2602688 L165.212631,13.2602688 L169.036377,21.547957 L173.44827,21.547957 L169.301279,12.5369785 C171.566093,11.2410968 172.565786,9.52326882 172.565786,6.84110752 C172.565786,5.03288172 171.948205,3.52605375 170.713043,2.29041935 C169.506789,1.05478495 168.036158,0.452043015 166.30115,0.452043015 L158.888602,0.452043015 L158.888602,21.547957 L162.859516,21.547957 Z M162.859516,4.36989246 L166.30115,4.36989246 C167.506878,4.36989246 168.595398,5.36437635 168.595398,6.90135484 C168.507097,8.95067742 167.095333,9.40272044 166.30115,9.34247312 L162.859516,9.34247312 L162.859516,4.36989246 Z M181.952491,4.36989246 L185.452467,4.36989246 C186.77593,4.36989246 187.658414,4.82193548 188.15826,5.69587097 C188.658632,6.56986022 188.923535,8.2273871 188.923535,10.6685054 C188.835234,16.5150753 187.923316,17.6301613 185.275865,17.6301613 L181.952491,17.6301613 L181.952491,4.36989246 Z M185.275865,21.547957 C190.364732,21.547957 192.893923,17.9315054 192.893923,10.6685054 C192.893923,3.85754838 190.423599,0.452043015 185.452467,0.452043015 L177.981577,0.452043015 L177.981577,21.547957 L185.275865,21.547957 Z M211.31045,21.547957 L211.31045,17.6301613 L206.721955,17.6301613 L206.721955,4.36989246 L211.31045,4.36989246 L211.31045,0.452043015 L198.133112,0.452043015 L198.133112,4.36989246 L202.751041,4.36989246 L202.751041,17.6301613 L198.133112,17.6301613 L198.133112,21.547957 L211.31045,21.547957 Z M221.314211,21.547957 L221.314211,8.01644086 L228.6968,21.547957 L232.315036,21.547957 L232.315036,0.452043015 L228.314688,0.452043015 L228.314688,13.9835591 L221.019874,0.452043015 L217.343297,0.452043015 L217.343297,21.547957 L221.314211,21.547957 Z M251.172016,21.547957 L251.172016,17.6301613 L242.112761,17.6301613 L242.112761,12.4164301 L250.3484,12.4164301 L250.3484,8.49863441 L242.112761,8.49863441 L242.112761,4.36989246 L251.172016,4.36989246 L251.172016,0.452043015 L238.141847,0.452043015 L238.141847,21.547957 L251.172016,21.547957 Z M261.411246,21.547957 L261.411246,13.2602688 L263.764361,13.2602688 L267.588107,21.547957 L272,21.547957 L267.85301,12.5369785 C270.117823,11.2410968 271.118042,9.52326882 271.118042,6.84110752 C271.118042,5.03288172 270.499935,3.52605375 269.264773,2.29041935 C268.059045,1.05478495 266.588414,0.452043015 264.85288,0.452043015 L257.440857,0.452043015 L257.440857,21.547957 L261.411246,21.547957 Z M261.411246,4.36989246 L264.85288,4.36989246 C266.058608,4.36989246 267.147128,5.36437635 267.147128,6.90135484 C267.058827,8.95067742 265.647063,9.40272044 264.85288,9.34247312 L261.411246,9.34247312 L261.411246,4.36989246 Z" id="Fill-7"></path>
             <polygon id="Fill-9" points="277 5 330 5 330 1 277 1"></polygon>
         </g>
@@ -362,7 +389,7 @@ map.on("load", async () => {
 
     // adding menu
 
-    // If these two layers were not added to the map, abort
+    // If these layers were not added to the map, abort
     if (!map.getLayer('1818') || !map.getLayer('1884') || !map.getLayer('1910') || !map.getLayer('2022')) {
         return;
     }
@@ -370,7 +397,7 @@ map.on("load", async () => {
     const toggleableLayerIds = ['1818', '1884', '1910', '2022'];
 
 
-    function zoomToLayerExtent(layerId) {
+    function zoomToLayerExtentShoreline(layerId) {
         //const source = map.getSource('shoreline_overlay');
         const layerFeatures = shoreline_geojson_data.features.filter(f => f.properties.year === Number(layerId));
         const bbox = turf.bbox({
@@ -416,7 +443,7 @@ map.on("load", async () => {
                 this.className = 'active';
 
                 if (clickedLayer != '2022') {
-                    zoomToLayerExtent(clickedLayer);
+                    zoomToLayerExtentShoreline(clickedLayer);
                 }
 
             } else {
@@ -611,41 +638,85 @@ map.on("load", async () => {
 
         new mapboxgl.Popup()
             .setLngLat(b_coordinates)
-            .setHTML("<h3>" + b_title + "</h3>" + b_artist + "<br>" + b_season + " " + b_year + "<br>" + "<img src='" + b_image + "'" + " class=popupImage " + "/>" + "<br>" + b_description)
+            .setHTML("<h3>" + b_title + "</h3>" + b_artist + "<br>" + b_season + " " + b_year + "<br>" + "<img src='" + b_image + "'" + " class=popupImage" + "/>" + "<br>" + b_description)
             .addTo(map);
     });
 
+    // TESTING
 
-    // INDIGENOUS TERRITORIES ZOOMING
-    const start = {
-        center: [-79.380331532, 43.646497454], // // starting center in [lng, lat]
-        zoom: 13.6,
+     // If these layers were not added to the map, abort
+     if (!map.getLayer('bentway')) {
+        return;
+    }
+    // Enumerate ids of the layers.
+    const PresentToggleableLayerIds = ['bentway'];
+
+    const PresentTagIdToTextContent = {
+        'bentway': 'The Bentway',
     };
 
-    const end = {
-        center: [-79.380331532, 43.646497454],
-        zoom: 7,
-    };
+
+    bentwayBounds = [[
+        -79.41142742172009,
+        43.63583355687243
+      ],[
+        -79.40292777889691,
+        43.638554567828066
+      ]]
+
+    // Set up the corresponding toggle button for each layer.
+    for (const id of PresentToggleableLayerIds) {
+        // Skip layers that already have a button set up.
+        if (document.getElementById(id)) {
+            continue;
+        }
+
+        // Create a link.
+        const link = document.createElement('a');
+        link.id = id;
+        link.href = '#';
+        link.textContent = PresentTagIdToTextContent[id];
+        link.className = '';
+
+        // Show or hide layer when the toggle is clicked.
+        link.onclick = function (e) {
+            const clickedLayer = this.id;
+            e.preventDefault();
+            e.stopPropagation();
+
+            const visibility = map.getLayoutProperty(
+                clickedLayer,
+                'visibility'
+            );
+
+            // Toggle layer visibility by changing the layout object's visibility property.
+            if (visibility === 'none') {
+                map.setLayoutProperty(clickedLayer, 'visibility', 'visible');
+                this.className = 'active';
+
+            } else {
+                this.className = '';
+                if (clickedLayer != 'bentway') { // if it isn't the bentway layer, turn off vis and set map extent to gardiner
+                    map.setLayoutProperty(
+                        clickedLayer,
+                        'visibility',
+                        'none'
+                    );
+                } else {
+                    map.fitBounds(bentwayBounds)
+                }
+            }
+        };
+
+        const layers = document.getElementById('present-menu');
+        layers.appendChild(link);
+    }
 
 
-    let isAtStart = true;
-
-    document.getElementById('fly').addEventListener('click', () => {
-        // Zoom out to show Indigenous Territories
-        const target = isAtStart ? end : start;
-        isAtStart = !isAtStart;
+    // FUTURE MAP
 
 
-        map.flyTo({
-            ...target,
-            center: [-79.380331532, 43.646497454], //NE coordinates
-            duration: 1000,
-            essential: true // this animation is considered essential with respect to prefers-reduced-motion
-        });
-    });
-
-
-    // TESTING ZONE
+    // TURNING ON AND OFF BASED ON PAST PRESENT FUTURE
 
     // Get the radio button elements
     const radios = document.querySelectorAll('[name="tabs"]');
@@ -656,13 +727,17 @@ map.on("load", async () => {
             if (radio.id === 'radio-1' && radio.checked) {
                 map.setLayoutProperty('bentway_photos', 'visibility', 'none'); // bw photos
                 map.setLayoutProperty('photos', 'visibility', 'visible'); // archival photos
+                map.setLayoutProperty('bentway', 'visibility', 'none'); // bentway
                 document.getElementById('tagmenu').style.display = 'flex';
                 document.getElementById('menu').style.display = 'flex';
+                document.getElementById('present-menu').style.display = 'none';
 
             } else if (radio.id === 'radio-2' && radio.checked) {
                 map.setLayoutProperty('bentway_photos', 'visibility', 'visible'); // bw
                 map.setLayoutProperty('photos', 'visibility', 'none'); // archival photos
                 map.setLayoutProperty('bentway', 'visibility', 'visible'); // bentway
+                document.getElementById('present-menu').style.display = 'flex';
+                document.getElementById('present-menu').style.top = '10px'; // move layers menu to top
                 document.getElementById('tagmenu').style.display = 'none';
                 document.getElementById('menu').style.display = 'none';
 
@@ -673,6 +748,4 @@ map.on("load", async () => {
         });
     });
 });
-
-
 
